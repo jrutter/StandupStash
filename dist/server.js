bd98feee92c67015b662ee38626a864c504b4c04
Jake Rutter <jake@onerutter.com>
2018-01-24 08:34:33 -0500

    Fixed server

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
+
+
 app = express();
+
+app.use(history({
+// verbose: true
+}));
+
 app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
