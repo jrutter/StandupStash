e89e6c11d3d6a2939507606433534ff866ec043d
Jake Rutter <jake@onerutter.com>
2018-01-25 06:59:59 -0500

    Config setup

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 var config = {};
 //development
+// https://stackoverflow.com/questions/40809317/where-to-store-api-keys-in-express-node-for-heroku
 var env = process.env.NODE_ENV || 'development';
 if(env === 'development'){
   //config = require('./env/development');
