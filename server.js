0418c54cfbb8ae8606b41ef199a06f8b4fb4f52e
Jake Rutter <jake@onerutter.com>
2018-01-30 21:42:31 -0500

    Hapijs config

--- server.js
+++ server.js
@@ -1,48 +1,14 @@
 'use strict';
 
 const Hapi = require('hapi');
-const Inert = require('inert');
-const Path = require('path');
 
-// Create a server with a host and port
-const server = Hapi.server({
-    port: 8000,
-    routes: {
-        files: {
-            relativeTo: Path.join(__dirname, '/dist')
-        }
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
     }
+    console.log(`Server running at: ${server.info.uri}`);
 });
-
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: function (request, h) {
-
-        return 'hello world';
-    }
-});
-
-const provision = async () => {
-
-    await server.register(Inert);
-
-    server.route({
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-                path: '.',
-                redirectToSlash: true,
-                index: true,
-            }
-        }
-    });
-
-    await server.start();
-
-    console.log('Server running at:', server.info.uri);
-};
-
-provision();
