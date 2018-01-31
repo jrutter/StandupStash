9092e7aa3b071f78b2372394c5e3e1c5cc84304a
Jake Rutter <jake@onerutter.com>
2018-01-30 21:46:56 -0500

    Fixed hapi config version

--- server.js
+++ server.js
@@ -2,8 +2,19 @@
 
 const Hapi = require('hapi');
 
-const server = new Hapi.Server();
-server.connection({ port: 3000, host: 'localhost' });
+
+const server = new Hapi.Server({
+  host: 'localhost',
+  port: 3000
+})
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
 
 server.start((err) => {
 
