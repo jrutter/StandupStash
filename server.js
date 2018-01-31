4a328c0ad669c8da5b5558390e6aee8fae8eb4fa
Jake Rutter <jake@onerutter.com>
2018-01-31 06:23:08 -0500

    Heroku

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var Hapi = require('hapi');
 
 var server = new Hapi.Server(process.env.PORT || 5000);
 
-// server.route({
-//     method: 'GET',
-//     path: '/hello',
-//     handler: function (request, reply) {
-//         reply('Hello, world!');
-//     }
-// });
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
 
 server.start((err) => {
 
