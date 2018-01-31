7c6cadb2147f752406bb66e50b6f07f5f1ac98d4
Jake Rutter <jake@onerutter.com>
2018-01-31 06:11:51 -0500

    Heroku Conf

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 
 var Hapi = require('hapi');
 
-var port = process.env.PORT || 5000;
-var server = new Hapi.Server(port);
+var server = new Hapi.Server(process.env.PORT || 5000);
 
 server.route({
     method: 'GET',
