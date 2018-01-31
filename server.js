588ff2f8749d6b1f8e4882a1e67a270de27e323e
Jake Rutter <jake@onerutter.com>
2018-01-30 22:30:01 -0500

    hapi config

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 var Hapi = require('hapi');
 
 var port = process.env.PORT || 5000;
-var server = new Hapi.Server(port, '0.0.0.0');
+var server = new Hapi.Server(port);
 
 server.route({
     method: 'GET',
-    path: '/',
+    path: '/hello',
     handler: function (request, reply) {
         reply('Hello, world!');
     }
