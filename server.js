446813cd6d5bbdd0449197308cd562051ca32d85
Jake Rutter <jake@onerutter.com>
2018-01-31 06:50:27 -0500

    Heroku Config

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
-var Path = require('path');
-var Hapi = require('hapi');
-var Inert = require('inert');
+const Path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
 
-var server = new Hapi.Server({
-    connections: {
-        routes: {
-            files: {
-                relativeTo: Path.join(__dirname, 'dist')
+const server = new Hapi.Server({
+    port: process.env.PORT || 8080,
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'public')
+        }
+    }
+});
+
+const provision = async () => {
+
+    await server.register(Inert);
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '.',
+                redirectToSlash: true,
+                index: true,
             }
         }
-    }
-});
-server.connection({ process.env.PORT || 8080 });
+    });
 
-server.register(Inert, function () {});
-
-server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-        directory: {
-            path: '.',
-            redirectToSlash: true,
-            index: true
-        }
-    }
-});
-
-server.start(function (err) {
-
-    if (err) {
-        throw err;
-    }
+    await server.start();
 
     console.log('Server running at:', server.info.uri);
-});
+};
+
+provision();
