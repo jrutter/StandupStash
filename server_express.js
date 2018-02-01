3c29d683cc55fcdc9a74e2720411683e4f2612ae
Jake Rutter <jake@onerutter.com>
2018-02-01 07:25:07 -0500

    Working on Express router

--- server_express.js
+++ server_express.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var itemRoutes = require('./server_routes');
 
-var serveStatic = require('serve-static');
 var history = require('connect-history-api-fallback');
 
 
@@ -13,8 +13,16 @@ app.use(bodyParser.json());
 app.use(history({
 // verbose: true
 }));
+// app.get('/api/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 
-app.use(serveStatic(__dirname + "/dist"));
+
+app.use(express.static('dist'));
+
+app.use('/blog-posts', itemRoutes);
+
+//routers/blogPostsRouter.js
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port);
---
+++ server_routes.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+// // Require Item model in our routes module
+// // var Item = require('../models/Item');
+// // Defined store route
+// itemRoutes.route('/test').post(function (req, res) {
+//   // var item = new Item(req.body);
+//       // item.save()
+//     // .then(item => {
+//     // res.status(200).json({'item': 'Item added successfully'});
+//     // })
+//     // .catch(err => {
+//     // res.status(400).send("unable to save to database");
+//     // });
+//     res.status(200).json({'item': 'Item added successfully'});
+//
+// });
