ff31aaa41b48f00bad5f28feaf6050d7a39336e6
Jake Rutter <jake@onerutter.com>
2018-02-01 07:10:05 -0500

    Reverted back to using expressjs

---
+++ server_hapi.js
@@ -0,0 +1,46 @@
+const Path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
+
+const server = new Hapi.Server({
+    port: process.env.PORT || 8080,
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'dist')
+        }
+    }
+});
+
+const provision = async () => {
+
+    await server.register(Inert);
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '.',
+                // redirectToSlash: true,
+                // index: true,
+            }
+        }
+    });
+
+    // Example api call
+    server.route({
+      method: 'GET',
+      path: '/api/call',
+      handler: function (request, h) {
+        return {
+          message: 'Hello!'
+        };
+      }
+    });
+
+    await server.start();
+
+    console.log('Server running at:', server.info.uri);
+};
+
+provision();
