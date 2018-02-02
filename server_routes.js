11ecda387b4fc7b8b4292a7d76b27c56651ebba2
Jake Rutter <jake@onerutter.com>
2018-02-02 11:07:05 -0500

    Cleaned up routes

--- server_routes.js
+++ server_routes.js
@@ -8,20 +8,3 @@ router.get('/api', (req, res) => {
 });
 
 module.exports = router;
-
-
-// // Require Item model in our routes module
-// // var Item = require('../models/Item');
-// // Defined store route
-// itemRoutes.route('/test').post(function (req, res) {
-//   // var item = new Item(req.body);
-//       // item.save()
-//     // .then(item => {
-//     // res.status(200).json({'item': 'Item added successfully'});
-//     // })
-//     // .catch(err => {
-//     // res.status(400).send("unable to save to database");
-//     // });
-//     res.status(200).json({'item': 'Item added successfully'});
-//
-// });
