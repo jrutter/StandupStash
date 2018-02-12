bc7d01de9b42212a2e88c6cad00cb4f9590614e7
Jake Rutter <jake@onerutter.com>
2018-02-12 06:46:02 -0500

    Day 44 - Still trying to figure out the routes

--- server_express.js
+++ server_express.js
@@ -2,35 +2,19 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const profileRoutes = require('./server_routes');
 const history = require('connect-history-api-fallback');
 
 const app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(require('connect-history-api-fallback')())
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(history({
+  verbose: true
+}));
 
-app.use('/api', profileRoutes);
-//
-// app.get('/', function (req, res, next) {
-//   res.sendfile('./dist/index.html')
-// })
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
+app.get('/api', (req, res) => res.send('Hello World!'))
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 var port = process.env.PORT || 8080;
 app.listen(port);
